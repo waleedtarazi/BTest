5fed2ca0cfff4fd8030dc16ef1983b17d66477d8
waleedtarazi <waleedaltarazy@gamil.com>
2025-05-15 16:27:57 +0300

    add/totalNoTodos

--- BridgeAITest/todo/serializers.py
+++ BridgeAITest/todo/serializers.py
@@ -2,6 +2,11 @@ from rest_framework import serializers
 from .models import Todo
 
 class TodoSerializer(serializers.ModelSerializer):
+    total_number_of_todos = serializers.SerializerMethodField()
+    
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'completed']
+        fields = ['id', 'title', 'completed', 'total_number_of_todos']
+        
+    def get_total_number_of_todos(self, todo):
+        return Todo.objects.count()
